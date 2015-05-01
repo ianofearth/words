1e90ebe9ce5f42d5eb6e99217274585d12e14597
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-01 10:18:31 -0700

    add and pass id spec

---
+++ lib/word.rb
@@ -0,0 +1,37 @@
+class Word
+	@@word = []
+
+	define_method(:initialize) do |instance|
+		@word = instance
+		@id = @@word.length().+(1)
+		@dictionary = []
+	end
+
+	define_method(:word) do
+		@word
+	end
+
+	define_method(:id) do
+		@id
+	end
+
+	# define_method(:dictionary) do
+	# 	@dictionary
+	# end
+
+	# define_singleton_method(:all) do
+	# 	@@word
+	# end
+
+	# define_method(:save) do
+	# 	@@word.push(self)
+	# end
+
+	# define_singleton_method(:clear) do
+	# 	@@word = []
+	# end
+ 
+
+
+
+end
---
+++ spec/word_spec.rb
@@ -0,0 +1,63 @@
+require('rspec')
+require('word')
+require('definition')
+
+describe(Word) do
+	# before() do
+	# 	Word.clear()
+	# end
+
+	describe('#word') do
+		it('returns a word entered') do
+			test_word = Word.new("ian")
+			expect(test_word.word()).to(eq("ian"))
+		end
+	end
+
+	describe('#id') do
+		it('returns the id of the word') do
+			test_word = Word.new("ian")
+			expect(test_word.id()).to(eq(1))
+		end
+	end
+
+	# describe('#dictionary') do
+	# 	it('initially returns an empty array of cars for the dictionary') do
+	# 		test_word = Word.new("ian")
+	# 		expect(test_word.dictionary()).to(eq([]))
+	# 	end
+	# end
+
+	# describe('.all') do
+	# 	it('is empty at first') do
+	# 		expect(Word.all()).to(eq([]))
+	# 	end
+	# end
+
+	# describe('#save') do
+	# 	it('adds a word to the array of saved words') do
+	# 		test_word = Word.new("ian")
+	# 		test_word.save()
+	# 		expect(Word.all()).to(eq([test_word]))
+	# 	end
+	# end
+
+	# describe('.clear') do
+	# 	it('empties out all saved words from the array') do
+	# 		Word.new("ian").save()
+	# 		Word.clear()
+	# 		expect(Word.all()).to(eq([]))
+	# 	end
+	# end
+
+	# describe('.find') do
+	# 	it('returns a word by its id number') do
+	# 		test_word = Word.new
+	# 	end
+	# end
+
+
+
+
+
+end
