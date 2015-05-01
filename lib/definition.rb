a501a86b95366c8820682a1f33189b5435387068
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-01 11:01:46 -0700

    add spec to test all method, pass spec for all method

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,1 +1,20 @@
+class Definition
+	@@definitions = []
 
+	define_method(:initialize) do |definition|
+		@definition = definition
+		@id = @@definitions.length().+(1)
+	end
+
+
+
+	# define_method(:definition) do
+	# 	@definition
+	# end
+
+	define_singleton_method(:all) do
+		@@definitions
+	end
+
+
+end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,20 +1,35 @@
 require('rspec')
 require('definition')
+require('word')
 
 describe(Definition) do
-	before() do
-		Definition.clear()
-	end
+	# before() do
+	# 	Definition.clear()
+	# end
 
-	describe('#definition') do
-		it('returns the definition of a word') do
-			test_definition = Definition.new("is really cool")
-			test_definition.save()
-			expect(test_definition.definition()).to(eq("is really cool"))
+	define_singleton_method('.all') do
+		it('is empty at first') do
+			expect(Definition.all()).to(eq([]))
 		end
 	end
 
+	# describe('#definition') do
+	# 	it('returns the definition of a word') do
+	# 		test_definition = Definition.new("is really cool")
+	# 		test_definition.save()
+	# 		expect(test_definition.definition()).to(eq("is really cool"))
+	# 	end
+	# end
+
+	# describe('#save') do
+	# 	it('adds a definition to the array of saved definitions') do
+	# 		test_definition = Definition.new("is really cool")
+	# 		test_definition.save()
+	# 		expect(Definition.all()).to(eq([test_definition]))
+	# 	end
+	# end
 
 
-	
+
+
 end
